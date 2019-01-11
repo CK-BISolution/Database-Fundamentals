f3d0612b1197140049ccae466ce6edc5908161ac
Ci1912 <zhuci27@yahoo.com>
2019-01-10 23:56:29 -0500

    Add files via upload
    Create a data type [Name] as string with length (50)

---
+++ TYPE [dbo].[Name].sql
@@ -0,0 +1,9 @@
+/****** Object:  UserDefinedDataType [dbo].[Name]    Script Date: 2019/1/9 0:03:50 ******/
+DROP TYPE [dbo].[Name]
+GO
+
+/****** Object:  UserDefinedDataType [dbo].[Name]    Script Date: 2019/1/9 0:03:50 ******/
+CREATE TYPE [dbo].[Name] FROM [nvarchar](50) NULL
+GO
+
+
